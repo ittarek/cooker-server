a73f10a92878747ed37dde6b140002d4ccb10159
MD Tariqul Islam <ittarek551@gmail.com>
2023-05-04 05:05:13 +0600

    server hase been create

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 const chefData = require("./data/Chef.json");
 const todayMenu = require("./data/TodayMenu.json");
-
+const detailsData = require("./data/detailsData.json");
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("cooker is running");
@@ -15,9 +15,16 @@ app.get("/chefData", (req, res) => {
   res.send(chefData);
 });
 
-// today Menu data
 app.get("/todayMenu", (req, res) => {
   res.send(todayMenu);
+});
+
+app.get("/chefData/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const chefRecipe = detailsData.filter(
+    (data) => parseInt(data.category_id) === id
+  );
+  res.send(chefRecipe);
 });
 
 app.listen(port, () => [console.log(`cooker api is running ${port}`)]);
